154ca7e03907dc632f9b93c51d3f374f766f2627
Vartijam <jami.vartiainen@student.hamk.fi>
2024-10-24 18:54:33 +0300

    Tasavuosikymmenet ja eläkelauseet lisätty

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int ika = 65;
+        int ika = 59;
 
         if (ika >= 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
@@ -17,5 +17,21 @@ public class App {
             } if (ika == 18) {
                 System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa");
         }
+        if (ika == 20 || ika == 30 || ika == 40 || ika == 50 || ika == 60 || ika == 70 || ika == 80 || ika == 90 || ika == 100 || ika == 110) {
+            System.out.println("Onneksi olkoon tasavuosikymmenistä!");
+        }
+        if (ika == 100) {
+            System.out.println("Onneksi olkoon!!!!");
+        }
+        if (ika >= 58 && ika < 65) {
+            System.out.println("Voit siirtyä varhaiseläkkeelle");
+        }
+        if (ika == 65) {
+            System.out.println("Hyviä eläkepäiviä!");
+        }
+        if (ika >= 40 && ika <= 50) {
+            System.out.println("Hyvää keski-ikää!");
+        }
+
     }
 }
