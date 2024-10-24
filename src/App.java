e5e9ceecd8e1d2faa62af872192d755a5f5d098e
Vartijam <jami.vartiainen@student.hamk.fi>
2024-10-24 18:41:05 +0300

    Alaikäisen if-lauseke lisätty

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        int ika = 50;
 
+        if (ika >= 0 && ika <= 18) {
+            System.out.println("Olet alaikäinen");
+            if (ika >= 15) {
+                System.out.println("Saat ajaa mopoa");
+            } if (ika >= 16 && ika < 18) {
+                System.out.println("Saat ajaa kevaria");
+            }
+        }
     }
 }
