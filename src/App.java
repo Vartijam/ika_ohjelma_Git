de8e3be547a54ba2e05617c6f6bd5bc777020685
Vartijam <jami.vartiainen@student.hamk.fi>
2024-10-24 18:48:32 +0300

    Aikuisuuden else if -lauseke lisätty

--- src/App.java
+++ src/App.java
@@ -1,15 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int ika = 50;
+        int ika = 65;
 
-        if (ika >= 0 && ika <= 18) {
+        if (ika >= 0 && ika < 18) {
             System.out.println("Olet alaikäinen");
             if (ika >= 15) {
                 System.out.println("Saat ajaa mopoa");
             } if (ika >= 16 && ika < 18) {
                 System.out.println("Saat ajaa kevaria");
             }
+        } else if (ika >= 65) {
+                System.out.println("Olet eläkeläinen");
+            } else {
+                System.out.println("Olet aikuinen");
+            } if (ika == 18) {
+                System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa");
         }
     }
 }
